55ed08879b3ebea4ace3e381503a508dcc4c32f2
Dan Aloni <dan@kernelim.com>
2016-01-18 07:11:40 +0200

    Remove links.sh, IDEs should be smart enough

--- links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create links to auto-generated Hs files in order to statisfy editor's automatic syntax checking.
-
-ln -s ../../../dist/build/gitomail/gitomail-tmp/Gitomail/Maintainers/Lexer.hs src/Gitomail/Maintainers
-ln -s ../../../dist/build/gitomail/gitomail-tmp/Gitomail/Maintainers/Parser.hs src/Gitomail/Maintainers
-ln -s ../../../dist/build/gitomail/gitomail-tmp/Lib/Lexer/Lexer.hs src/Lib/Lexer
