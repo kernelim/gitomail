ba72c741ce4be680a61ae66d1159001cadc66f47
Dan Aloni <dan@kernelim.com>
2016-05-09 21:52:17 +0300

    Make mkdocs work and add a helper script

---
+++ mkdocs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$1" == "setup" ]] ; then
+    sudo dnf install libyaml-devel python-virtualenv
+    virtualenv docenv
+    ./docenv/bin/pip install mkdocs==0.15.3
+elif [[ "$1" == "upload" ]] ; then
+    ./docenv/bin/mkdocs gh-deploy --clean
+elif [[ "$1" == "serve" ]] ; then
+    ./docenv/bin/mkdocs serve
+fi
+
